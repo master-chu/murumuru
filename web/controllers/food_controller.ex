d887239e8a49a2edc347fbf44532f41397555671
erika <eoblena@zipcar.com>
2016-03-01 14:51:10 -0500

    create a show controller action

--- web/controllers/food_controller.ex
+++ web/controllers/food_controller.ex
@@ -1,4 +1,7 @@
 defmodule Murumuru.FoodController do
   use Murumuru.Web, :controller
 
+  def index(conn, _params) do
+    json conn, %{id: 353}
+  end
 end
--- web/router.ex
+++ web/router.ex
@@ -1,27 +1,13 @@
 defmodule Murumuru.Router do
   use Murumuru.Web, :router
 
-  pipeline :browser do
-    plug :accepts, ["html"]
-    plug :fetch_session
-    plug :fetch_flash
-    plug :protect_from_forgery
-    plug :put_secure_browser_headers
-  end
-
   pipeline :api do
     plug :accepts, ["json"]
   end
 
   scope "/", Murumuru do
-    # pipe_through :browser # Use the default browser stack
-    pipe_through :json # Use the default browser stack
+    pipe_through :api # Use the default browser stack
 
-    resources "/foods", FoodController, only: [:show]
+    resources "/foods", FoodController, only: [:index]
   end
-
-  # Other scopes may use custom stacks.
-  # scope "/api", Murumuru do
-  #   pipe_through :api
-  # end
 end
